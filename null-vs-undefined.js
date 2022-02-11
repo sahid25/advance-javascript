332de65c77e891128f2cf57bfb4c61bb9bf18e88
sahid25 <ashrafsahid25@gmail.com>
2022-02-11 10:28:29 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,9 @@
+// let outPut = undefined;
+// let outPut;
+// let outPut = {name: 'someOne' , phone:239823}
+// function thisNum(num,num2){ console.log(num + num2);}
+// function thisNum(num,num2){ console.log(num + num2); return;}
+// let outPut = thisNum(23);
+// let outPut = [23,22,21];
+// console.log(outPut[33]);
+console.log(outPut);
---
+++ trueOrFalse.js
@@ -0,0 +1,13 @@
+// const name = 0; // condition will be false 
+// const name = 1;  // condition will be true
+// const name = ''; // condition will be false
+// const name = ' '; // condition will be true
+// const name = {}; // condition will be True
+// const name = []; // condition will be true
+// const name = 'jhon wike'; // condition will be true
+if(name){
+    console.log('condition will be true');
+}
+else{
+    console.log('condition will be false');
+}
